41476c6134d60746da64cf75f61b27b0bbc1b9a4
i.didenko <didenko.i.a@effective-group.ru>
2022-10-01 20:52:16 +0300

    code style

--- bot.js
+++ bot.js
@@ -1,19 +1,16 @@
-const { mineflayer: mineflayerViewer } = require('prismarine-viewer')
+// const { mineflayer: mineflayerViewer } = require('prismarine-viewer')
 const inventoryViewer = require('mineflayer-web-inventory')
 const { AgentBuilder } = require('./lib/mineflayer-agent')
 
-const Agent = AgentBuilder();
+const Agent = AgentBuilder()
 Agent.config = {
   host: '176.119.159.250',
-  username: 'agent-007',
+  username: 'agent-007'
 }
 
-const bot = Agent.connection();
+const bot = Agent.connection()
 
 bot.once('spawn', async () => {
   // mineflayerViewer(bot, { port: 3007, firstPerson: true }) // port is the minecraft server port, if first person is false, you get a bird's-eye view
   inventoryViewer(bot, {})
 })
-
-
-

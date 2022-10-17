336dd0cfb9253264931a46aad4d505f61c3f74d4
i.didenko <didenko.i.a@effective-group.ru>
2022-10-18 02:36:29 +0300

    TakeItemChecker

--- bot.js
+++ bot.js
@@ -9,6 +9,7 @@ Agent.config.username = 'agent-007'
 const bot = Agent.connection()
 
 bot.once('spawn', async () => {
+  console.log(bot.entity.position)
   // mineflayerViewer(bot, { port: 3007, firstPerson: true }) // port is the minecraft server port, if first person is false, you get a bird's-eye view
   inventoryViewer(bot, {})
 })
